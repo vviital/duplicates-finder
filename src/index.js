bac6b44215d0d8335f68a526dd331aa85d65d0f6
vviital <vviital@tut.by>
2018-02-24 12:32:13 +0300

    Write report into report.json file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+import fs from 'fs';
+import Promise from 'bluebird';
+import path from 'path';
+
+const baseName = process.env.BASE_NAME || __dirname;
+
 import { duplicateFinder } from './duplicates-finder';
 
 (async () => {
-  const iterator = duplicateFinder('/Users/vviital/projects/learning/js/')();
+  const iterator = duplicateFinder(baseName)();
+
+  const object = {};
 
   for await (const value of iterator) {
-    console.log('value', value);
+    const key = JSON.parse(value[0]);
+    object[key.filename] = { count: value[1], size: key.size };
   }
-})().then(console.log).catch(console.error);
+
+  const report = JSON.stringify(object, ' ', 1);
+  const reportName = path.join(__dirname, '..', 'report.json');
+
+  await Promise.promisify(fs.writeFile, { context: fs })(reportName, report);
+})().catch(console.error);
